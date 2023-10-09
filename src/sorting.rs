dee5faa2d25614693bfe29d03e8a824fe556289e
Pablo Huijse <pablo.huijse@gmail.com>
2023-10-09 08:23:11 +0200

    Renaming and improving docs

--- src/sorting.rs
+++ src/sorting.rs
@@ -1,29 +1,37 @@
-/// Returns the indexes that sort a `data`. Works with floating point vectors.
+//! Module with sorting functions
+
+/// Returns the indexes that sort the floating point array `data`.
+///
+/// # Examples
 /// ```
-/// use rustroperiods::sorting::argsort;
+/// # use rustroperiods::sorting::argsort;
 /// let input: Vec<f64> = vec![2.0, 1.0, 3.0];
-/// let result: Vec<usize> = vec![1, 0, 2];
-/// assert_eq!(argsort(&input), result);
+/// let result = argsort(&input);
+/// assert_eq!(argsort(&input), vec![1, 0, 2]);
 /// ```
+///
 pub fn argsort(data: &[f64]) -> Vec<usize> {
     let mut indices = (0..data.len()).collect::<Vec<_>>();
-    indices.sort_by(|&i, &j| data[i].total_cmp(&data[j]));
+    indices.sort_unstable_by(|&i, &j| data[i].total_cmp(&data[j]));
     indices
 }
 
-/// Returns the indexes of the `npeaks` highest local maxima in `data`
+/// Returns the indexes of the `npeaks` highest local maxima in the floating point array `data`.
+///
+/// # Examples
 /// ```
-/// use rustroperiods::sorting::find_peaks;
+/// # use rustroperiods::sorting::find_peaks;
 /// let input: Vec<f64> = vec![0.0, 1.0, 2.0, 1.0, 0.0];
-/// let result: Vec<usize> = vec![2];
-/// assert_eq!(find_peaks(&input, 1), result);
+/// let result = find_peaks(&input, 1);
+/// assert_eq!(result, vec![2]);
 /// ```
+///
 pub fn find_peaks(data: &[f64], npeaks: usize) -> Vec<usize> {
     assert!(npeaks > 0);
     let mut local_maxima = (1..data.len() - 1)
         .filter(|&i| (data[i] >= data[i - 1]) & (data[i] >= data[i + 1]))
         .collect::<Vec<usize>>();
     assert!(npeaks <= local_maxima.len());
-    local_maxima.sort_by(|&i, &j| data[i].total_cmp(&data[j]));
+    local_maxima.sort_unstable_by(|&i, &j| data[i].total_cmp(&data[j]));
     local_maxima[0..npeaks].to_vec()
 }
--- src/stats.rs
+++ src/stats.rs
@@ -1,26 +1,39 @@
+//! the stats module contains
+
 /// Returns the average/sample mean of a floating point array
 ///
+/// # Example
 ///```
-///use rustroperiods::stats::average;
-///let input = [1.0, 2.0, 3.0];
-///assert!((average(&input) -  2.0).abs() < f64::EPSILON);
+/// # use rustroperiods::stats::sample_mean;
+/// let result = sample_mean(&[1.0, 2.0, 3.0]);
+/// assert!((result -  2.0).abs() < f64::EPSILON);
 ///```
 #[inline(always)]
-pub fn average(data: &[f64]) -> f64 {
+pub fn sample_mean(data: &[f64]) -> f64 {
+    assert!(data.len() > 0);
     let sum = data.into_iter().sum::<f64>();
     sum / data.len() as f64
 }
 
 /// Returns the variance of a floating point array
 ///
+/// # Example
 /// ```
-/// use rustroperiods::stats::variance;
-/// let input = [1.0, 2.0, 3.0];
-/// assert!((variance(&input) - 1.0).abs() < f64::EPSILON)
+/// # use rustroperiods::stats::sample_variance;
+/// let result = sample_variance(&[1.0, 2.0, 3.0], true);
+/// assert!((result - 1.0).abs() < f64::EPSILON)
 /// ```
 #[inline(always)]
-pub fn variance(data: &[f64]) -> f64 {
-    let average = average(data);
-    let sum_of_squares = data.into_iter().map(|xi| (xi - average).powi(2)).sum::<f64>();
-    sum_of_squares / (data.len() as f64 - 1.0)
+pub fn sample_variance(data: &[f64], unbiased: bool) -> f64 {
+    assert!(data.len() > 0);
+    let average = sample_mean(data);
+    let sum_of_squares = data
+        .into_iter()
+        .map(|xi| (xi - average).powi(2))
+        .sum::<f64>();
+    let mut denominator = data.len() as f64;
+    if unbiased {
+        denominator -= 1.0;
+    }
+    sum_of_squares / denominator
 }

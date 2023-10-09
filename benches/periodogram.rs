9b5cac9c981c57d9d46ef0ccfedc5537aa057ec8
Pablo Huijse <pablo.huijse@gmail.com>
2023-10-09 08:23:45 +0200

    benchmark periodogram as a function of the number of samples

--- benches/periodogram.rs
+++ benches/periodogram.rs
@@ -1,15 +1,23 @@
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::*;
 use rustroperiods::lightcurve::LightCurve;
 use rustroperiods::periodograms::string_length;
 
 pub fn periodogram_benchmark(c: &mut Criterion) {
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     let mut group = c.benchmark_group("periodograms");
+    group.plot_config(plot_config);
     group.sample_size(10);
-    let n = [10, 20, 50, 100, 200, 500, 1000];
-    let lc = LightCurve::random(n[3]);
-    group.bench_function("string length", |bencher| {
-        bencher.iter(|| string_length(&lc, 1e-3, 3.0, 1e-4))
-    });
+    group.sampling_mode(SamplingMode::Flat);
+    for n in [10, 20, 50, 100, 200, 500, 1000].iter() {
+        let lc = LightCurve::random(*n as usize);
+        group.bench_with_input(BenchmarkId::from_parameter(n), n, |b, &n| {
+            b.iter(|| string_length(black_box(&lc), 1e-3, 3.0, 1e-4))
+        });
+    }
+    group.finish();
+    // group.bench_function("string length", |bencher| {
+    //     bencher.iter(|| string_length(black_box(&lc), 1e-3, 3.0, 1e-4))
+    // });
 }
 
 criterion_group!(benches, periodogram_benchmark);

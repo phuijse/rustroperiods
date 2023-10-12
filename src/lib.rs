2634c91773c920e3ae4ea6dcaccad11298719ba8
Pablo Huijse <pablo.huijse@gmail.com>
2023-10-12 19:19:39 +0200

    use ndarrays instead of lists

--- src/lib.rs
+++ src/lib.rs
@@ -8,54 +8,42 @@
 //! irregularly sampled in time (non-constant time between observations) and because of this, the
 //! FFT cannot be used. Fortunately, the astronomical and signal processing communities have
 //! proposed several alternatives in the past decades.
-//! The periodograms implemented in this crate are not estimators of the PSD, but they do work in a
-//! similar way, in the sense that they can be interpreted as the likelihood of a given oscillation
-//! frequency being present in the time series. All the periodograms are computed using a brute
-//! force strategy, evaluating an user-defined grid of candidate frequencies. Please see the
-//! documentation in the module `periodograms` for information on the methods included in this
-//! crate.
-
+//! The periodograms implemented in this crate are not estimators of the PSD per-se, but they do
+//! work in a similar way, in the sense that they can be interpreted as the likelihood of a given
+//! oscillation frequency being present in the time series. All the periodograms are computed using
+//! a brute force strategy, i.e. evaluating an user-defined grid of candidate frequencies. Please
+//! see the documentation in the module `periodograms` for information on the methods included
+//! in this crate.
+//! This crate uses the PyO3 ecosystem of tools to provide interfaces for Python 3. It is intended
+//! as a replacement for the P4J python/cython library by the same author of this crate.
 pub mod lightcurve;
 pub mod periodograms;
 pub mod sorting;
 pub mod stats;
-//use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
+use numpy::{IntoPyArray, PyArray1, PyReadonlyArray2};
+use numpy::{PyArray, PyArrayDyn, PyReadonlyArrayDyn};
+use periodograms::{single_band_periodogram, Periodogram};
 use pyo3::prelude::*;
-
-pub enum Periodogram {
-    StringLength,
-}
 
 #[pyfunction]
 #[pyo3(name = "single_band_periodogram")]
-fn single_band_periodogram_py(mjd: Vec<f64>, mag: Vec<f64>, err: Vec<f64>) -> PyResult<Vec<f64>> {
-    let lc = lightcurve::LightCurve { mjd, mag, err };
-    let val = single_band_periodogram(&lc, Periodogram::StringLength);
-    Ok(val)
+fn single_band_periodogram_py<'py>(
+    py: Python<'py>,
+    mjd: PyReadonlyArrayDyn<'py, f64>,
+    mag: PyReadonlyArrayDyn<'py, f64>,
+    err: PyReadonlyArrayDyn<'py, f64>,
+) -> PyResult<&'py PyArray1<f64>> {
+    let lc = lightcurve::LightCurve {
+        mjd: mjd.to_vec().unwrap(),
+        mag: mag.to_vec().unwrap(),
+        err: err.to_vec().unwrap(),
+    };
+    let per = single_band_periodogram(&lc, Periodogram::StringLength, 1e-3, 3.0, 1e-4);
+    Ok(PyArray::from_vec(py, per))
 }
 
 #[pymodule]
 fn rustroperiods(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(single_band_periodogram_py, m)?)?;
     Ok(())
-}
-
-pub fn single_band_periodogram(lc: &lightcurve::LightCurve, method: Periodogram) -> Vec<f64> {
-    let statistic = match method {
-        Periodogram::StringLength => periodograms::lafler_kinman_string_length(&lc),
-    };
-    periodograms::sweep_frequency_grid(statistic, 1e-3, 3.0, 1e-4)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::lightcurve::LightCurve;
-    use crate::{single_band_periodogram, Periodogram};
-
-    #[test]
-    fn it_works() {
-        let lc = LightCurve::noisy_sinewave(1000, 100.0, 10.0, 0.2);
-        single_band_periodogram(&lc, Periodogram::StringLength);
-        assert_eq!(false, true);
-    }
 }
--- src/periodograms.rs
+++ src/periodograms.rs
@@ -4,6 +4,29 @@ use crate::lightcurve::LightCurve;
 use crate::sorting::argsort;
 use crate::stats;
 
+/// The periodogram algorithms available
+pub enum Periodogram {
+    StringLength,
+}
+
+/// Computes a given periodogram over a range of linearly-spaced frequencies starting in `fmin`,
+/// ending in `fmax` and separated by `fstep`
+pub fn single_band_periodogram(
+    lc: &LightCurve,
+    method: Periodogram,
+    fmin: f64,
+    fmax: f64,
+    fstep: f64,
+) -> Vec<f64> {
+    let statistic = match method {
+        Periodogram::StringLength => lafler_kinman_string_length(&lc),
+    };
+    let nsteps = ((fmax - fmin) / fstep) as i32;
+    (0..nsteps)
+        .map(|k| statistic(f64::from(k).mul_add(fstep, fmin)))
+        .collect()
+}
+
 /// Performs the epoch folding transformation
 /// phase = modulo(time, period)/period
 /// on each element of `times`
@@ -15,32 +38,16 @@ fn fold(times: &[f64], period: f64) -> Vec<f64> {
         .collect()
 }
 
-/// Computes a given periodogram statistic over a range of linearly-spaced frequencies starting in
-/// `fmin`, ending in `fmax` and separated by `fstep`
-pub fn sweep_frequency_grid<F>(statistic: F, fmin: f64, fmax: f64, fstep: f64) -> Vec<f64>
-where
-    F: Fn(f64) -> f64,
-{
-    let nsteps = ((fmax - fmin) / fstep) as i32;
-    let mut periodogram: Vec<f64> = Vec::with_capacity(nsteps as usize);
-    for k in 0..nsteps {
-        let trial_frequency = f64::from(k).mul_add(fstep, fmin);
-        periodogram.push(statistic(trial_frequency));
-    }
-    periodogram
-}
-
 /// The String Length Lafler-Kinman (SLLK) statistic corresponds to Eq. 9 in David Clarke,
 /// "String/Rope length methods using the Lafler-Kinman statistic", Astronomy & Astrophysics, vol.
 /// 386, n. 2, pp. 763-774, 2002.
 ///
 /// Reference: <https://ui.adsabs.harvard.edu/abs/2002A%26A...386..763C/abstract>
 ///
-pub fn lafler_kinman_string_length<'a>(lc: &'a LightCurve) -> impl Fn(f64) -> f64 + 'a {
+fn lafler_kinman_string_length<'a>(lc: &'a LightCurve) -> impl Fn(f64) -> f64 + 'a {
     let n_samples = lc.mag.len() as f64;
     let mag_variance = stats::sample_variance(&lc.mag, true);
     let normalizing_constant = 2.0 * n_samples * mag_variance;
-    println!("How many times do I say this?");
     move |trial_frequency| {
         let phase: Vec<f64> = fold(&lc.mjd, trial_frequency.powi(-1));
         let folded_magnitude = argsort(&phase)
